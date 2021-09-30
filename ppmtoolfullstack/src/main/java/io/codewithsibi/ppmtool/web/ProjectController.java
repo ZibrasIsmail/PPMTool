bd5998cbfec3b4f5e2c8775e703fd57399600556
ZibrasIsmail <sibras.18@itfac.mrt.ac.lk>
2021-09-30 18:03:53 +0530

    Update Controller

---
+++ ppmtoolfullstack/src/main/java/io/codewithsibi/ppmtool/web/ProjectController.java
@@ -0,0 +1,40 @@
+package io.codewithsibi.ppmtool.web;
+
+import io.codewithsibi.ppmtool.domain.Project;
+import io.codewithsibi.ppmtool.services.MapValidationErrorService;
+import io.codewithsibi.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
+        if(errorMap!=null) return errorMap;
+
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
